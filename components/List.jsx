e9e9ef6d4eb522c84de616de895c3dd1bd868a6f
dangrt5 <randy.dang05@gmail.com>
2019-02-04 15:44:56 -0800

    New todo routing added

---
+++ components/List.jsx
@@ -0,0 +1,5 @@
+export default () => (
+    <div className="list">
+        <h1>List Goes Here</h1>
+    </div>
+)
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
 import Head from "../components/Head";
+import List from '../components/List';
+import Link from "next/link";
 
 export default () => {
     return (
         <Head title='Next.js Todo List!'>
-            <h1>Next Todo List</h1>
-            <div className="todo-list">
-                <h1>ToDO ITEM</h1>
-            </div>
+            <h1 className="text-center">Next Todo List 💩</h1>
+            <List />
             <div className="nav-buttons">
-                <button className="btn btn-primary">New Todo List</button>
+                <Link href="/new">
+                    <button className="btn btn-primary">New Todo List</button>
+                </Link>
             </div>
         </Head>
 
