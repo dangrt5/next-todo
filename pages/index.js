4b3ca83e8aa540e1fd9f19d6fbd620d0f3fadb76
dangrt5 <randy.dang05@gmail.com>
2019-02-04 14:53:05 -0800

    added head element and bootstrap

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
+import Head from "../components/Head";
+
 export default () => {
     return (
-        <div>
-            Next TODO LIST
-        </div>
+        <Head title='Next.js Todo List!'>
+            <h1>Next Todo List</h1>
+            <div className="todo-list">
+                <h1>ToDO ITEM</h1>
+            </div>
+            <div className="nav-buttons">
+                <button className="btn btn-primary">New Todo List</button>
+            </div>
+        </Head>
+
     )
 };
